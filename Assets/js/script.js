832728d9ea99196108b2392ce40e1b8772595ccc
Anthony Santo <anthonysanto@anthonys-mbp.lan>
2022-08-06 20:58:33 -0600

    Takes user input to get length of password

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,25 @@
 var password = document.getElementById("password");
 
+function passwordLength(){
+    var passwordLength = prompt("Pick a password length between 8 and 128");
+
+    if (passwordLength < 8){
+        alert("Please pick a password longer than 8 characters")
+    }
+    else if (passwordLength > 128){
+        alert("Please pick a password shorter than 128 characters")
+    }
+    else {
+        return passwordLength;
+    }
+}
+
 function getPassword(){
-    var passLength = 14; 
+    var length = passwordLength();
     var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz?!@$%^&*()";
     var password = "";
 
-    for (var i = 0; i <= passLength; i++) {
+    for (var i = 0; i <= length; i++) {
         var randomNumber = Math.floor(Math.random() * characters.length);
         password += characters.substring(randomNumber, randomNumber + 1);
     }
